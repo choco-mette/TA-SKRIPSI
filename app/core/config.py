db32097059e38617b78fae60573486ee8e8cb967
AchmadMuafiTaufiqurrochman <withdraw14@gmail.com>
2025-10-15 21:55:07 +0700

    Refactor database configuration to use individual environment variables for connection settings

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    DB_URL: str = os.getenv("DATABASE_URL")
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_PORT: str = os.getenv("DB_PORT")
     API_LLM_URL: str = os.getenv("API_LLM_URL")
     MODEL_LLM_NAME: str = os.getenv("MODEL_LLM_NAME")
 
--- app/db/database.py
+++ app/db/database.py
@@ -2,5 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-engine = create_engine(settings.DB_URL)
+database_url = f"postgresql+psycopg2://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+engine = create_engine(database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
